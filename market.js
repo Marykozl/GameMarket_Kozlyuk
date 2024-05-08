8d496939e3c820f7ba185857435c7551a1142dd3
Marykozl <158029846+Marykozl@users.noreply.github.com>
2024-05-08 11:20:47 +0300

    08/05

--- market.js
+++ market.js
@@ -6,18 +6,27 @@ let itemsDiv = document.getElementById("items");
 if (itemsDiv) 
     {
     //Вивід знайденого елементу
-    console.log(itemsDiv)
+    // console.log(itemsDiv)
     //додавання відформатованого HTML коду в блок
     //itemsDiv.innerHTML+=' <div class="item"></div>'
     //itemsDiv.innerHTML+=' <div class="item"></div>'
     //itemsDiv.innerHTML+=' <div class="item"></div>'
     //itemsDiv.innerHTML+=' <div class="item"></div>'
-    for (let i = 0; i < 100; i++)
-        {
-        itemsDiv.innerHTML += '<div class = "item"></div>'
-    }
+    //for (let i = 0; i < 100; i++)
+      //  {
+        //itemsDiv.innerHTML += '<div class = "item"></div>'
+    //}
     }
      else {
     //Вивід повідомлення про не знайдений блок
     console.log("Блок товарів не знайдено")
 }
+//визначення масиву товарів
+let itemsArray = ['Газонокосарка 43','Електричний тример 110','Електрична газонокосарка 32','Акумуляторний оприскувач 12 N']
+//виведення в консоль масиву
+console.log(itemsArray)
+//
+for (let i = 0; i < itemsArray.length; i++)
+    {
+        console.log(itemsArray[i])
+    }
