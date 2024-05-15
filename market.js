3a7c205532ee4f5b9048fc7854ed91639b5752b3
Marykozl <158029846+Marykozl@users.noreply.github.com>
2024-05-15 09:53:02 +0300

    15/05/2

--- market.js
+++ market.js
@@ -1,15 +1,37 @@
 //Перевірка підключеного файлу скриптів 
     //console.log('Перевірка підключеного файлу скриптів market.js')
 //визначення масиву товарів
-    let itemsArray =   ['Газонокосарка 43',
-                        'Електричний тример 110',
-                        'Електрична газонокосарка 32',
-                        'Акумуляторний оприскувач 12 N',
-                        'Газонокосарка 430',
-                        'Електричний тример 130',
-                        'Електрична газонокосарка 320',
-                        'Акумуляторний оприскувач 12 E',
-                       ]
+    let itemsArray =   [
+{
+    firstName: "Туш для вій",
+    lastName: "Maybelline New York Lash Sensational Sky High",
+    photo: "https://u.makeup.com.ua/f/fy/fyu3okmsetsw.jpg",
+},
+{
+    firstName: "Блиск для губ",
+    lastName: "Maybelline New York Lifter Gloss",
+    photo: "https://mykady.com/cdn/shop/products/q_0f0fa1b1-e63f-4dd3-86c0-93d00fad9be4.jpg?v=1668081431",
+},
+{
+    firstName: "Підводка для очей",
+    lastName: "Maybelline New York Hyper Easy",
+    photo: "https://www.maybelline.ua/~/media/mny/global/eye-makeup/eyeliner/eyestudio-hyper-easy-liquid-eyeliner-eye-makeup/maybelline-hyper-easy-liquid-liner-black-041554578607-o.jpg?thn=0&w=380&hash=6ACF6BC59C3521CC6841C75B3A21F2653432494C",
+},
+{
+    firstName: "Хайлайтер у формі стіку",
+    lastName: "Maybelline Master Strobing",
+    photo: "https://cdn.notinoimg.com/detail_zoom/maybelline/3600531342265_01-o/maybelline-master-strobing___170523.jpg",
+},
+    ]
+                        //['Газонокосарка 43',
+                        //'Електричний тример 110',
+                        //'Електрична газонокосарка 32',
+                        //'Акумуляторний оприскувач 12 N',
+                        //'Газонокосарка 430',
+                        //'Електричний тример 130',
+                        //'Електрична газонокосарка 320',
+                        //'Акумуляторний оприскувач 12 E',
+                       //]
 // Отримання елементу з ідентифікатором items 
     let itemsDiv = document.getElementById("items");
 //Перевірка існування знайденого блоку
@@ -18,10 +40,11 @@
 //виведення елементів масиву
     itemsArray.forEach((item, index) =>
 {
-    itemsDiv.innerHTML += `<div class = "item">
-    <footer>
-    <h2>Товар №${index+1} з ${itemsArray.length}</h2>
-    <p>${item}</p>
+    itemsDiv.innerHTML += 
+    `<div class = "item">
+    <h2>Товар № ${index+1} з ${itemsArray.length}</h2>
+    <p>${item.lastName} ${item.firstNameName}</p>
+    <p><img src="${item.photo}" alt="${item.lastName} ${item.firstName}" class="user-photo"></p>
     </footer>
     </div>`
     
